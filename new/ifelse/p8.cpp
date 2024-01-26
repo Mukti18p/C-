6a75ff2df77c59e7744d1b249276949aef5210a3
Mukti18p <155235925+Mukti18p@users.noreply.github.com>
2024-01-26 15:37:44 +0000

    char is uppercase or lowercase

---
+++ new/ifelse/p8.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin>>ch;
+
+    if(ch<='a' && ch>='z')
+    {
+        cout<<"char is lowercase\n";
+    }
+    else
+    {
+        cout<<"char is uppercase\n";
+    }
+
+    
+
+}
