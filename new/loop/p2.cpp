0219a8820d134923994a75303eb4593f9c665baa
Mukti18p <155235925+Mukti18p@users.noreply.github.com>
2024-01-26 15:53:44 +0000

    sum of natural nums

---
+++ new/loop/p2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum=0,i=1;
+
+    for(i=1;i<=10;i++)
+    {
+        cout<<i<<endl;
+        sum=sum+i;
+    }
+    cout<<"sum is "<<sum<<endl;
+}
