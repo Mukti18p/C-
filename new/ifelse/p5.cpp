efde64ec5a7ad3522a8bf0ade342cc6d39751090
Mukti18p <155235925+Mukti18p@users.noreply.github.com>
2024-01-26 15:26:58 +0000

    print positive number

---
+++ new/ifelse/p5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+
+    if(x<0)
+    {
+        cout<<"num is "<<x*(-1)<<endl;
+    }
+    else{
+        cout<<"num is "<<x<<endl;
+    }
+}
