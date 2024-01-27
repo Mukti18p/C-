9f81d0d0555c67fa36dd1c4563b4b4987bf886e8
Mukti18p <155235925+Mukti18p@users.noreply.github.com>
2024-01-27 05:38:24 +0000

    pascal traiangle

---
+++ new/loop/p24.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int fact(int a)
+{
+    int j=1;
+    for(int i=a;i>=1;i--)
+    {
+        j=j*i;
+    }
+    return j;
+}
+int main()
+{
+    int n,a,b,c;
+    cin>>n;
+    float d=0;
+    for(int i=0;i<n;i++)
+    {
+        d=0;
+        for(int j=0;j<=i;j++)
+        {
+            a=fact(i);
+            b=fact(j);
+            c=fact(i-j);
+            if(b==0  || c==0)
+            {
+                d=1;
+            }
+            else
+            {
+            d=(float)a/(b*c);
+            }
+            cout<<d;
+        }
+        cout<<endl;
+    }
+}
