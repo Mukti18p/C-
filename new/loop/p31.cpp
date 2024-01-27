ac53c07a177b5dd5b4567308ef69a153f541de40
Mukti18p <155235925+Mukti18p@users.noreply.github.com>
2024-01-27 06:15:04 +0000

    pattern printing

--- new/loop/p31.cpp
+++ new/loop/p31.cpp
@@ -1,14 +1,32 @@
 #include<iostream>
+#include<math.h>
+
 using namespace std;
 int main()
 {
-    int n,b=0;
+    int n,b=0,a=0;
     cin>>n;
     for(int i=0;i<n;i++)
     {
+        a=-1;
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<" ";
+        }
         for(int j=0;j<2*i+1;j++)
         {
-                
+              if(j<=i)
+              {
+                a++;
+                b=pow(2,a);
+                cout<<b;
+              }
+              else{
+                a--;
+                b=pow(2,a);
+                cout<<b;
+              }
         }
+        cout<<endl;
     }
 }    
