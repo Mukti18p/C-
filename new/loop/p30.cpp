b04229b89addc6fc53e9ea950e95ffea00cd00d7
Mukti18p <155235925+Mukti18p@users.noreply.github.com>
2024-01-27 06:08:59 +0000

    pattern print

---
+++ new/loop/p30.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,a=0;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        a=i+1;
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=0;j<=i;j++)
+        {
+                cout<<a;
+                a--;5
+        }
+        cout<<endl;
+    }
+}
