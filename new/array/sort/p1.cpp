1637bee911b529e5c47524325718dfcaff17e142
Mukti18p <155235925+Mukti18p@users.noreply.github.com>
2024-01-28 10:10:45 +0000

    bubble sort

---
+++ new/array/sort/p1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+void swap(int *a,int *b)
+{
+    int *temp=0;
+    *temp=*a;
+    *a=*b;
+    *b=*temp;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    int i=0,j=0;
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+       
+            cout<<arr[i]<<" ";
+            
+    }
+}
+//binary search
