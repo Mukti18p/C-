20d20ac2357356fc0b84fdc985836c847842fd9e
Mukti18p <155235925+Mukti18p@users.noreply.github.com>
2024-01-28 08:21:10 +0000

    enter kth largest ele in array

---
+++ new/array/p2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int sort(int n,int arr[])
+{
+    int i;
+    int max=-1;
+    for(i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                max=arr[j];
+                arr[j]=arr[i];
+                arr[i]=max;
+            }
+        }
+    }
+}
+int main()
+{
+    int i,n;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(n,arr);
+    int k;
+    cout<<"enter k ele";
+    cin>>k;
+    cout<<arr[k-1];
+}
