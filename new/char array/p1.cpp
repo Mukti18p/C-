fc7fcd2b85d07423d2ef82b4786e32566be2e677
MUKTI18 <155235925+Mukti18p@users.noreply.github.com>
2024-02-08 16:13:05 +0000

    null

---
+++ new/char array/p1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    char a[10];
+    cin>>a;
+    cout<<a;
+    int i,j=0;
+    i=0;
+    int n=0;
+    while(a[n]!='\0')
+    {
+        n++;
+    }
+    j=n-1;
+    int count=0;
+    while(i<=j)
+    {
+       if(a[i]==a[j])
+       {
+        count=1;
+        continue;
+       }
+       else
+       {
+        count=2;
+        break;
+       }
+    }
+    if(count==2)
+    {
+        cout<<"not palindrome";
+    }
+    else
+    {
+        cout<<"palin drome";
+    }
+
+   
+}
