3dde9c479f68f610a2d2b3d45a878c8e8fbde182
Mukti18p <mukti2216patel@gmail.com>
2024-02-19 09:49:25 +0530

    null

---
+++ streak/day2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,i,j,k;
+	cin>>n;
+	int a[n];
+	int q;
+	cin>>q;
+	for(i=0;i<n;i++)
+	{
+	    cin>>a[i];
+	}
+	for(i=0;i<q;i++)
+	{
+		int l,r;
+		cin>>l>>r;
+		    
+	}
+}
