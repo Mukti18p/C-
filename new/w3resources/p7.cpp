e8d57a126e2e7da0f4ef723de58749cd3be58b7b
Mukti18p <mukti2216patel@gmail.com>
2024-02-22 00:42:08 +0530

    return num near to 100

--- new/w3resources/p7.cpp
+++ new/w3resources/p7.cpp
@@ -1,11 +1,32 @@
 #include<iostream>
 #include<string>
 using namespace std;
+int fun(int num,int num1)
+{
+    int dif=100-num;
+    int diff=100-num1;
+    if(dif<0)
+    {
+        dif=-dif;
+    }
+    if(diff<0)
+    {
+        diff=-diff;
+    }
+    if(dif<diff)
+    {
+        return num;
+    }
+    else
+    {
+        return num1;
+    }
+}
 int main()
 {
-    string s;
-    getline(cin,s);
-    int a = s.find("C#",0,2);
-    cout<<a+1<<endl;
+    int num,num1;
+    cin>>num>>num1;
+    int a=fun(num,num1);
+    cout<<a<<endl;
 
-}
+}    
