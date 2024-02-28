909d85a595ecbfb56a3d5a80c2fcef7f8988dfd8
Mukti18p <mukti2216patel@gmail.com>
2024-02-28 22:14:06 +0530

    read 5 students marks and name and then use array of pointer while sorting there name

--- class/p2.cpp
+++ class/p2.cpp
@@ -1,39 +1,67 @@
 #include<iostream>
 #include<cstring>
+#include<limits>
 using namespace std;
+
 class fun{
     private:
         string name;
         int a[5];
-
-
     public:
-    void getdata();
-    void ptr(int n,fun *a);
-
+        void getdata();
+        void ptr(int n, fun *b[]);
+        void putdata();
 };      
-void   fun::getdata() {
-    cout<<"Enter name";
-    getline(cin,name);
+
+void fun::putdata() {
+    cout<<"Name : "<<name<<endl;
+    for(int i=0; i<5; i++) {
+        cout<<"Element["<<i+1<<"] = "<<a[i]<<endl;
+    }
+}
+
+void fun::getdata() {
+    cout<<"Enter name: ";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+    getline(cin, name);
     cout<<"Enter marks :"<<endl;
-    for(int i=0;i<5;i++)
-    {
+    for(int i=0; i<5; i++) {
         cin>>a[i];
     }
 }
-void ptr(int n,fun *a)
-{
-    
-}
-int main()
-{
-        int n,i,j;
-        cin>>n;
-        fun *p = new fun[n];
-        for(i=0; i<n;i++)
-        {
-            p[i].getdata();
+
+void fun::ptr(int n, fun *b[]) {
+    fun *p1;
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            if(b[i]->name.compare(b[j]->name) > 0) {
+                p1 = b[i];
+                b[i] = b[j];
+                b[j] = p1;
+            }
         }
-       p[0].ptr(n,&p[0]);
-       
+    }
+}
+
+int main() {
+    int n;
+    cout<<"Enter the number of students: ";
+    cin>>n;
+    
+    fun *p[n];
+    for(int i=0; i<n; i++) {
+        p[i] = new fun;
+    }
+
+    for(int i=0; i<n; i++) {
+        p[i]->getdata();
+    }
+    
+    p[0]->ptr(n, p);
+    
+    for(int j=0; j<n; j++) {
+        p[j]->putdata();
+    }
+    
+    return 0;
 }
