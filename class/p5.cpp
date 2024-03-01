299397bdeeb2cc5a614590a73bd45658c704f8d3
Mukti18p <mukti2216patel@gmail.com>
2024-03-01 20:40:50 +0530

    null

--- class/p5.cpp
+++ class/p5.cpp
@@ -1,7 +1,148 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 #include<limits>
+using namespace std;
+
+class student1;
+class student
+{
+private:
+   string name;
+    int roll;
+
+public:
+    void getdata();
+    void putdata();
+    friend int find(int, student *[], student1 *[]);
+    friend void swap(int a,int b,student *[],student1 *[]);
+};
+
+class student1
+{
+private:
+    string name;
+    int roll;
+
+public:
+    void getdata();
+    void putdata();
+    friend int find(int, student *[], student1 *[]);
+    friend void swap(int a,int b,student *[],student1 *[]);
+};
+
+void student::getdata()
+{
+     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin,name);
+    cin >> roll;
+}
+
+void student::putdata()
+{
+    cout << "Name: " << name << endl;
+    cout << "Roll no = " << roll << endl;
+}
+
+void student1::getdata()
+{
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin,name);
+    cin >> roll;
+}
+
+void student1::putdata()
+{
+    cout << "Name: " << name << endl;
+    cout << "Roll no = " << roll << endl;
+}
+
+int find(int n, student *p[], student1 *q[])
+{
+    int a=0,b=0;
+    string temp;
+    for (int i = 0; i < n; i++)
+    {
+        temp=p[i]->name;
+        if(temp.compare("Jhon")==0)
+        {
+             a=i;
+            break;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+
+       temp=q[i]->name;
+        if(temp.compare("mukti")==0)
+        {
+             b=i;
+            break;
+        }
+    }
+    if (a != -1 && b != -1) {
+        swap(a,b,p,q);
+    }
+    return 0;
+}
+
+void swap(int a,int b,student *p[],student1 *q[])
+{
+    int temp=p[a]->roll;
+    p[a]->roll=q[b]->roll;
+    q[b]->roll=temp;
+   
+    string t;
+    t=p[a]->name;
+    p[a]->name=q[b]->name;
+    q[b]->name=t;
+}
+
 int main()
 {
-    
+    int n = 5;
+    cin >> n;
+    student *p[n];
+    for (int i = 0; i < n; i++)
+    {
+        p[i] = new student;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        p[i]->getdata();
+       
+    }
+    for (int i = 0; i < n; i++)
+    {
+        p[i]->putdata();
+    }
+    student1 *q[n];
+    for (int i = 0; i < n; i++)
+    {
+        q[i] = new student1;
+       
+    }
+     for (int i = 0; i < n; i++)
+    {
+        q[i]->getdata();
+       
+    }
+    for (int i = 0; i < n; i++)
+    {
+        q[i]->putdata();
+    }
+    find(n, p, q);
+    cout<<endl<<"after swapping"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        p[i]->putdata();
+    }
+    for (int i = 0; i < n; i++)
+    {
+        q[i]->putdata();
+    }
+    // Deallocate dynamically allocated memory
+    for (int i = 0; i < n; i++) {
+        delete p[i];
+        delete q[i];
+    }
 }
---
+++ class/p6.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cstring>
+#include <limits>
+using namespace std;
+
+class abc {
+private:
+    int x;
+    int y;
+    static int x1;
+    static int y1;
+
+public:
+    void getdata();
+    void putdata();
+    static abc multi(abc n, abc n1);
+    static void put( abc &c);
+    abc sum(abc n, abc n1);
+    void subs(abc &n, abc &n1, abc &sub);
+};
+
+int abc::x1 = 0;
+int abc::y1 = 0;
+
+void abc::getdata() {
+    cout << "Enter the value of x :";
+    cin >> x;
+    cout << "Enter the value of y : ";
+    cin >> y;
+}
+
+void abc::putdata() {
+    cout << "The values are \nX = " << x << "\nY = " << y;
+}
+
+abc abc::sum(abc n, abc n1) {
+    abc s;
+    s.x = n.x + n1.x;
+    s.y = n.y + n1.y;
+    return s;
+}
+
+void abc::subs(abc &n, abc &n1, abc &sub) {
+    sub.x = n.x - n1.x;
+    sub.y = n.y - n1.y;
+}
+
+abc abc::multi(abc n, abc n1) {
+    abc c;
+    c.x1 = n.x * n1.x;
+   
+    c.y1 = n.y * n1.y;
+     c.y1 *= -1;
+    return c;
+}
+
+void abc::put( abc &c) 
+{
+    cout << "x=" << c.x1 << endl;
+    cout << "y=" << c.y1 << endl;
+}
+
+int main() {
+    abc n, n1, sum, sub, c;
+
+    n.getdata();
+    n1.getdata();
+
+    n.putdata();
+    n1.putdata();
+
+    sum = n.sum(n, n1);
+    sum.putdata();
+
+    n.subs(n, n1, sub);
+    sub.putdata();
+
+    c = abc::multi(n, n1);
+    abc::put(c);
+
+    return 0;
+}
