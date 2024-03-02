1b1d856dd84d36005d1cae5a96f043b598a13c7c
Mukti18p <mukti2216patel@gmail.com>
2024-03-02 23:27:57 +0530

    null

--- new/dymeallo/p3.cpp
+++ new/dymeallo/p3.cpp
@@ -7,5 +7,51 @@ struct st{
 };
 int main()
 {
-    
+    int r,c;
+    cin>>r>>c;
+    int **ip;
+    ip = (int **)malloc(sizeof(int *) * r); //allocating memory for rows 
+    for(int i=0;i<r;i++)
+    {
+        ip[i]=(int *)malloc(sizeof(int) * c);
+    }
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cin>>*(*(ip+i)+j);
+        }
+    }
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cout<<ip[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
+    cout<<"second array: "<<endl;
+    int *iq[r];
+    for(int i=0;i<r;i++)
+    {   
+            *(iq+i)=(int *)malloc(sizeof(int)*c);
+    }   
+     for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cin>>*(*(iq+i)+j);
+        }
+    }
+     for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cout<<iq[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
+
 }
