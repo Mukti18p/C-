23630679c9e458c7cc4b37f689b3ef83112aec6d
Mukti18p <mukti2216patel@gmail.com>
2024-03-04 23:46:45 +0530

    null

--- class/p7.cpp
+++ class/p7.cpp
@@ -1,10 +1,43 @@
 #include<iostream>
 #include<cstring>
 #include<limits>
+using namespace std;
+class xyz;
 class abc
 {
+    int a;
+    int b;
+    public:
+    friend void set(abc **,int );
+
 };
+class xyz
+{
+    int a;
+    int b;
+    public:
+    
+
+};
+void set(abc **p ,int n)
+{
+    cout<<"enter input";
+    for(int i=0;i<n;i++)
+    {
+        cin>>p[i]->a;
+        cin>>p[i]->b;
+    }
+}
 int main()
 {
+    int n;
+    cin>>n;
+    abc **ip,s;
+    ip=new abc*[n];
+    for(int i=0;i<n;i++)
+    {
+       ip[i]=new abc[2];
+    }
+    set(ip,n);
 
 }
