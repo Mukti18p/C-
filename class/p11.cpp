604e3658bf4939f240420a1d7b9769fe4686aa0a
Mukti18p <mukti2216patel@gmail.com>
2024-03-15 20:42:19 +0530

    null

--- class/p11.cpp
+++ class/p11.cpp
@@ -2,10 +2,47 @@
 using namespace std;
 class abc{
     private:
-        
+        int **p;
     public:
+    abc(){}
+    abc(int m,int n){
+        p = new int *[m];
+        for(int i=0;i<m;i++)
+        {
+            p[i]=new int [n];
+        }
+    }
+    void getdata(int i,int j){
+            cin>>p[i][j];
+    }
+    int putdata(int i,int j)
+    {
+        return p[i][j];
+    }
+
 };
 int main()
 {
+    int m,n;
+    cin>>m;
+    cin>>n;
+    abc p(m,n);
+    cout<<"Enter the elements of array : "<<endl;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            p.getdata(i,j);
+        }
+    }
+
+    
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<p.putdata(i,j);
+        }
+    }
 
 }
---
+++ class/p12.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<cstring>
+#include<limits>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int m=0,n;
+    cin>>n;
+    string s1;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin,s1);
+    int len = s1.length();
+    for(int i=0;i!=len;i++)
+    {
+        if((s1.at(i) == s1.at(i-1) || s1.at(i) == s1.at(i+1)) && i!=0  && i!=(len-1))
+        {
+            m++;
+        }
+        else if(i==0 && s1.at(i) == s1.at(i+1))
+        {
+            m++;
+        }
+        else if(i==len-1 && s1.at(i) == s1.at(i-1))
+
+        {
+            m++;
+           
+        }
+        else
+        {
+            continue;
+        }
+    }
+    if(m==n)
+    {
+        cout<<"YES"<<endl;
+        cout<<s1<<endl;
+    }
+    else
+    {
+        m=0;
+        n=0;
+        cout<<"NO"<<endl;
+    }
+    }
+
+
+}
