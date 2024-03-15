d72e934d49177dd3974f2a8e57fbdd51e76ecf70
Mukti18p <mukti2216patel@gmail.com>
2024-03-15 19:52:25 +0530

    null

--- class/p10.cpp
+++ class/p10.cpp
@@ -1,11 +1,27 @@
 #include<iostream>
 using namespace std;
 class abc{
+    int v;
+    int x;
     public:
-            
+        abc(int z){
+            v=z;
+            x=z;
+        }
+        abc(abc *p){
+            v = p->v;
+            x = p->x;
+
+        }
+        void display(){
+            cout<<"Value of v : "<<v<<endl;
+            cout<<x<<endl;
+        }
 };
 int main()
 {
-    
+    abc a (5);
+    abc b(&a);
+    b.display();
 
 }
